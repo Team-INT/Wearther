0df4ae0103407aeaab36882770950a57cb115309
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-11-10 23:32:51 +0900

    [test]_Self: 미들웨어 원복

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,21 @@
-import {auth} from "./auth";
-import {NextRequest, NextResponse} from "next/server";
+import { getSession } from "@/server/auth"
+import { NextRequest, NextResponse } from "next/server"
+import { match } from "path-to-regexp"
+
+const matchersForAuth = [
+  '/recommend/*',
+]
+
+const matchersForSignIn = [
+  '/register/*', 
+  '/login/*'
+]
+
 export async function middleware(request: NextRequest) {
   // 인증이 필요한 경로와 일치
-  // if(isMatch(request.nextUrl.pathname, matchersForAuth)){
-  //   return (await getSession()) ? NextResponse.next() : NextResponse.redirect(new URL('/login', request.url))
-  // }
-
-  const session = await auth();
-  // if (!session) {
-  //   return NextResponse.redirect("http://localhost:3000");
-  // }
+  if(isMatch(request.nextUrl.pathname, matchersForAuth)){
+    return (await getSession()) ? NextResponse.next() : NextResponse.redirect(new URL('/login', request.url))
+  }
 
   // 인증 후 회원가입/로그인 접근 제한
   // if(isMatch(request.nextUrl.pathname, matchersForSignIn)){
@@ -17,5 +23,9 @@ export async function middleware(request: NextRequest) {
   //   return (await getSession()) ? NextResponse.redirect(new URL('/', request.url)) : NextResponse.next()
   // }
 
-  return NextResponse.next();
+  return NextResponse.next()
+}
+
+function isMatch(pathname: string, urls: string[]){
+  return urls.some(url => !!match(url)(pathname))
 }
