eaf89836731c0d90d381c844faad2776010d4c25
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-11-21 11:18:34 +0900

    [feat]_self: 기본 메타데이터 추가

---
+++ src/lib/constants/metaData.ts
@@ -0,0 +1,7 @@
+export const baseDomain = "https://www.wearther.com";
+
+export const baseTitle = "Wearther - 날씨에 맞는 옷 추천 서비스";
+export const baseDescription =
+  "Wearther는 현재 날씨에 최적화된 패션 추천을 제공합니다. 나의 스타일과 기후에 맞는 옷차림을 손쉽게 찾아보세요. 오늘 뭐 입지 고민하지 마세요, Wearther가 도와드릴게요!";
+export const baseKeywords =
+  "날씨, 옷 추천, 패션, 기후, 옷차림, 스타일, 패션 트렌드, 코디, 날씨별 옷, Wearther";
